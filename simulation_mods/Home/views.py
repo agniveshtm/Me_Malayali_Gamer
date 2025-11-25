328489470347445824e576d0cca00fb7ba80437e
Agnivesh Manojkumar <agniveshtm2724@gmail.com>
2025-11-25 22:29:19 +0530

    Implement YouTube video integration on home page, including latest video display and error handling

--- simulation_mods/Home/views.py
+++ simulation_mods/Home/views.py
@@ -4,6 +4,8 @@ from .filters import PublicModsFilter
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from django.core.paginator import Paginator
+from django.conf import settings
+from googleapiclient.discovery import build
 #=============HOME PAGE===============#
 #============= DISPLAY HOME PAGE ===============#
 def home_page(request):
@@ -14,8 +16,9 @@ def home_page(request):
     paginator = Paginator(filtered_mods,5)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
-    return render(request,'main/index.html',{"public_mods":page_obj,"filter":mods_filter,"page_obj":page_obj,"current_order":order})
-
+    youtube_data = youtube_video(request)
+    return render(request,'main/index.html',{"public_mods":page_obj,"filter":mods_filter,"page_obj":page_obj,"current_order":order,
+                                             "latest_video":youtube_data['latest_video'],"trending_video":youtube_data['trending_video']})
 def download_count(request,pk):
     mod = get_object_or_404(Modsinfo,pk=pk)
     session_key = f'downloaded_mod_{pk}'
@@ -23,6 +26,7 @@ def download_count(request,pk):
         mod.downloads +=1
         mod.save()
         request.session[session_key]=True
+        request.session.modified = True
     return redirect(mod.download_link)
 
 @require_POST
@@ -40,8 +44,37 @@ def like_count(request,pk):
         mod.save()
         request.session[session_key]=True
         liked = True
-
+    request.session.modified = True
     return JsonResponse({'success':True,'likes':mod.likes,'liked':liked})
     
 def youtube_video(request):
-    pass
+    try:
+        youtube = build('youtube','v3',developerKey = settings.YOUTUBE_API_KEY)
+        channel_response = youtube.channels().list(part = 'contentDetails',id = settings.YOUTUBE_CHANNEL_ID).execute()
+        uploads_playlist_id = channel_response['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+
+        videos_response = youtube.playlistItems().list(
+            part = 'snippet',
+            playlistId = uploads_playlist_id,
+            maxResults = 1
+        ).execute()
+
+        if videos_response['items']:
+            video_id = videos_response['items'][0]['snippet']['resourceId']['videoId']
+            stats_response = youtube.videos().list(
+                part='statistics,snippet',
+                id=video_id
+            ).execute()
+                
+            latest_video = stats_response['items'][0] if stats_response['items'] else None
+                
+            return {
+                    'latest_video': latest_video,
+                    'trending_video': None   
+            }
+            
+        return {'latest_video': None, 'trending_video': None}
+    
+    except Exception as e:
+        print(f"Youtube API Error:: {e}")
+        return {'latest_video':None,'trending_video':None}
