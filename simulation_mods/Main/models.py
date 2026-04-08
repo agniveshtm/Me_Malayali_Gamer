a20463948a268df7dc5f6a167e3ae4a52e137398
Agnivesh Manojkumar <agniveshtm2724@gmail.com>
2026-04-08 10:05:11 +0530

    Combined functions into one

--- simulation_mods/Main/models.py
+++ simulation_mods/Main/models.py
@@ -77,10 +77,7 @@ def _delete_instance_files(instance, using=None):
             if file_to_delete and file_to_delete.name != field.get_default():
                 transaction.on_commit(lambda f=file_to_delete: f.delete(save=False), using=using)
 
-@receiver(post_delete, sender=Modsinfo)
-def delete_mod_images(sender, instance, using, **kwargs):
-    _delete_instance_files(instance, using=using)
-
-@receiver(post_delete, sender=Profile)
-def delete_profile_image(sender, instance, using, **kwargs):
+@receiver(post_delete, sender=[Modsinfo, Profile])
+def delete_files_on_model_delete(sender, instance, using, **kwargs):
+    """Signal handler to delete associated files when a model instance is deleted."""
     _delete_instance_files(instance, using=using)
